6b5b900d723aa55d4b008eae79a3e00666805b94
marc <marc.diaz.7e8@itb.cat>
2026-03-18 09:29:19 +0100

    boton mejor

--- app/src/main/java/com/example/camera_kit/MainActivity.kt
+++ app/src/main/java/com/example/camera_kit/MainActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.camera_kit.navigation.MainNavigationWrapper
 import com.example.camera_kit.navigation.Routes
 import com.example.camera_kit.ui.theme.CamerakitTheme
@@ -33,12 +37,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen(navigateToArScreen: (Routes) -> Unit) {
-    Button(
-        onClick = { navigateToArScreen(Routes.ArScreen(
-            lensId = "",
-            grouLensId = ""
-        ) ) },
-        content = { Text("Pantalla AR") }
-    )
+    Column(modifier = Modifier.padding(40.dp)){
+        Button(
+            onClick = { navigateToArScreen(Routes.ArScreen(
+                lensId = "",
+                grouLensId = ""
+            ) ) },
+            content = { Text("Pantalla AR") }
+        )
+    }
 }
 
